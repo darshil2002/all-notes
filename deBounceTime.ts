40a45ca62b3f0b8e783f59494eaef41b487f78e9
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-03-14 11:01:34 +0530

    debounceTime and using @viewchild to catch input

---
+++ deBounceTime.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+ 
+  map,
+  fromEvent,
+  debounceTime,
+} from 'rxjs';
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'],
+})
+
+export class MainComponent implements AfterViewInit {
+
+  @ViewChild('searchedValue') searchedValue!: ElementRef;
+
+  constructor(){}
+  Data: any;
+  ngAfterViewInit(): void {
+    const searchTerm=fromEvent<any>(this.searchedValue.nativeElement, 'keyup')
+
+    searchTerm.pipe(map(res=>res.target.value),debounceTime(1000)).subscribe(res=>{
+      this.Data=res;
+      console.log(res);
+    })
+  }
+
+}
+
+// html 
+
+
+// <input type="text" name="searchedValue" id="searchedValue" #searchedValue placeholder="search here...">
+// <br><br>
+// <span>your vale is :<span>{{this.Data}}</span></span>
