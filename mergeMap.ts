c188dcafcea47fa30e22899531021165686208bf
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-03-15 15:56:06 +0530

    mergeMap

---
+++ mergeMap.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AsyncSubject, Observable, ReplaySubject, concat, merge,from, of, map, mergeMap } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'y';
+
+
+  getData(returnVal:any){
+    return of(returnVal + ' is my friend')
+  }
+  ngOnInit(){
+
+    let myArray=['darshil','suchit','kaushal'];
+
+    // from(myArray).pipe(map(x=>this.getData(x))).subscribe(y=>y.subscribe(z=>{
+    //   console.log(z);
+    // })
+    // )
+    from(myArray).pipe(mergeMap(res=>this.getData(res))).subscribe(res=>{
+      console.log(res);
+      
+      let myLi=document.createElement('li');
+      myLi.innerText=res;
+      document.getElementById('myUl')?.appendChild(myLi);
+    })
+  }
+}
+
+// merge map thi if same thi ans in observable avto hoy to ene normal rite batai sakay
